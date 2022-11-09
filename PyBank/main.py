318d14dcd350e5490ee5d0cabff068564f1c7995
vincentgordon97 <66184004+vincentgordon97@users.noreply.github.com>
2022-11-09 10:57:44 -0500

    Calculated the greatest increase and greatest decrease in Profit/Loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import os 
 import csv
 
-csvpath = os.path.join('..','Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath, newline='') as csvfile:
     
@@ -14,9 +14,11 @@ with open(csvpath, newline='') as csvfile:
     dataset = [row for row in csvreader]
     
     # Find the total number of months in dataset
+    date = []
     months = []
     for row in dataset:
-        months.append(row[0][0:3])        
+        date.append(row[0])
+        months.append(row[0][0:3]) 
         total_months = len(months)
         
     print(f'Total Months: {total_months}')
@@ -35,15 +37,44 @@ with open(csvpath, newline='') as csvfile:
         profit_loss.append(row[1])
     
         # Get difference betwen numbers in profit_loss list
-        change_list = []
-        for x,y in zip(profit_loss[0::], profit_loss[1::]):
-            change_list.append(int(y) - int(x))    
-               
+        change_list = []  
+        for i in range(1,len(profit_loss)):
+            change_list.append(int(profit_loss[i]) - int(profit_loss[i-1]))
+            
     # Average of "Profit/Loss" changes
     change_total = sum(change_list)
     change_length = len(change_list)
     average_change = round((change_total / change_length), 2)
     
     print(f'Average Change: ${average_change}')
-        
-    # Greatest increase in profits
+   
+    
+    # Find the greatest increase in profits
+    max_value = change_list[0]
+    for number in change_list[1:]:
+        if number > max_value:
+            max_value = number
+            
+    # Find the greatest decrease in profits
+    min_value = change_list[0]
+    for number in change_list[1:]:
+        if number < min_value:
+            min_value = number
+    
+    # Find the month that caused the greatest increase in profits 
+    for i in range(len(profit_loss)):
+        if int(profit_loss[i]) - int(profit_loss[i - 1]) == max_value:
+            great_increase = date[i]
+            print(f"Greatest Increase in Profits: {great_increase} (${max_value})")
+     
+    # Find the month that caused the greatest decrease in profits 
+    for i in range(len(profit_loss)):     
+        if int(profit_loss[i]) - int(profit_loss[i - 1]) == min_value:
+           great_decrease = date[i] 
+           print(f"Greatest Decrease in Profits: {great_decrease} (${min_value})")
+
+    
+    
+    
+            
+    
